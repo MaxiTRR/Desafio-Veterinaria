9801c0dae1b25ea7b746cbd0473879528b4f5f66
MaxiTRR <maxihtorres@gmail.com>
2022-04-26 16:03:20 -0300

    v1.1.3 (modificacion de algunos estilos, cambio de los iconos svg por <i>, se agrego el modo dark)

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,6 @@
 //index.html
 
-function displayAlert(){
+ function displayAlert(){
     let message ="Las suscripciones se habilitaran pronto!"
     alert(message);
     let pregunta = confirm("¿Quire dejarnos alguna consulta?");
@@ -9,4 +9,32 @@ function displayAlert(){
     } else {
         location.href() 
     }
-}
+};
+
+//contacto.html
+
+function enviarForm(){
+    let pregunta =confirm("Estás seguro de querer enviar este mensaje?");
+    if (pregunta) {
+        let nombre = document.getElementById("nombre").value;
+        let mensaje = `Gracias por tu mensaje, ${nombre}`;
+        alert(mensaje);
+    }
+};
+
+//nosotros.html
+//Dark Mode
+
+function switchSheet() {
+    let theme = document.getElementById("theme");
+  
+    if (theme.getAttribute("href") == "css/mododia.css") {
+      theme.href = "css/modonoche.css";
+    } else {
+      theme.href = "css/mododia.css";
+    }
+  };
+
+
+
+
